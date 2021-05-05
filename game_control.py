6ad5a8d197c317e5617adc1be7c34be8fef015fc
benji <pughbenji@gmail.com>
2021-05-05 17:27:02 -0400

    Modified control code to allow pytests

--- game_control.py
+++ game_control.py
@@ -49,36 +49,47 @@ class MouseControl(Control):
     """
     A cursor based controller for Logisti Co. game.
     """
+
+    def __init__(self, gameboard):
+        super(MouseControl, self).__init__(gameboard)
+        self.events = []
+        self.mouse_pos = ()
     
+
     def control(self):
+        self.get_events()
+        self.get_mouse_pos()
         click = self.detect_click()
         self.tower_placement(click)
         self.tower_removal(click)
         
     def detect_click(self):
-        for event in pygame.event.get():
+        for event in self.events:
             if event.type == MOUSEBUTTONDOWN:
                 print(event.button)
                 return event.button
         return 0
     
+    def get_events(self):
+        self.events = pygame.event.get()
+
+    def get_mouse_pos(self):
+        self.mouse_pos = pygame.mouse.get_pos()
 
     def tower_placement(self, click):
         """
-        Attempt to place a tower where clicked
+        Attempt to place a tower where clicked.
         """
         if click == 1:
-            mouse_position = pygame.mouse.get_pos()
-            if 0<=mouse_position[0]<=800 and 0<=mouse_position[1]<=600:
-                self._gameboard.generate_tower(mouse_position[0], \
-                mouse_position[1], 300, 100)
+            if 0<=self.mouse_pos[0]<=800 and 0<=self.mouse_pos[1]<=600:
+                self._gameboard.generate_tower(self.mouse_pos[0], \
+                self.mouse_pos[1], 300, 100)
     
     def tower_removal(self, click):
         """
         Attempt to remove a tower where clicked.
         """
         if click == 3:
-            mouse_position = pygame.mouse.get_pos()
-            clicked_towers = [tower for tower in self._gameboard._robots if tower._rect.collidepoint(mouse_position)]
+            clicked_towers = [tower for tower in self._gameboard._robots if tower._rect.collidepoint(self.mouse_pos)]
             for tower in clicked_towers:
                 self._gameboard.remove_tower(tower)
